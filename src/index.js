86b5eb5f065fbb9417f6b4286c1ac1b376ecc132
lvaldenebro <lauravaldeal@gmail.com>
2022-12-20 11:23:20 +0100

    4.2 Express I 20/12

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('../web/src/data/movies.json');
 
 // create and config server
 const server = express();
@@ -8,6 +9,12 @@ server.use(express.json());
 
 // init express aplication
 const serverPort = 4000;
-server.listen(serverPort, () => {
+server.listen(serverPort, () => { //ejecuta el servidor
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+//END POINTS
+//For us: end points are dynamic routes where we specify the access point when someone asks for something. "Come here and do whatever is defined  into the callback function"
+server.get('/movies', (req, res) => {
+  res.json(movies); // ti seems that we don't need to return a data structure with success true, as it is already defined in movies.json
+})
